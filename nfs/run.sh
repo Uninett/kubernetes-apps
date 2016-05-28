b0f905da466f594b469caa3a8ce677038a5b5b69
Gurvinder Singh <gurvinder.singh@uninett.no>
2016-05-28 18:33:05 +0200

    updated spark apps with toree kernel

--- nfs/run.sh
+++ nfs/run.sh
@@ -6,6 +6,10 @@ service nfs-kernel-server start
 
 echo "Started"
 
+echo "Creating exports directries $EXPORT_DIRS"
+cd /exports
+mkdir -p $EXPORT_DIRS
+
 echo "Done all tasks - Running continious loop to keep this container alive"
 while true; do
   sleep 3600
--- spark/dockerfiles/jupyterhub/README.md
+++
@@ -1,3 +0,0 @@
-To run ipython notebook:
-
-docker run -d -p 80:8888 -v /notebooks:/notebooks -e "PASSWORD=daas" -e "USE_HTTP=1" uninett-daas/notebook-spark
