abf61f8eec2024029023c9d0d51c3566ba80d1f2
Gurvinder Singh <gurvinder.singh@uninett.no>
2016-10-19 10:44:01 +0200

    updated spark version and added some defaults for spark to make it support multiple users in same cluster

--- spark/dockerfiles/master/start-master.sh
+++ spark/dockerfiles/master/start-master.sh
@@ -9,6 +9,7 @@ echo "spark.ui.reverseProxyUrl  ${SPARK_PUBLIC_DNS}" >> $SPARK_HOME/conf/spark-d
 echo "SPARK_PUBLIC_DNS=${SPARK_PUBLIC_DNS}" > $SPARK_HOME/conf/spark-env.sh
 
 unset SPARK_MASTER_PORT
+mkdir -p $SPARK_RECOVERY_DIR
 
 # Run spark-class directly so that when it exits (or crashes), the pod restarts.
 $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master --webui-port 8080
